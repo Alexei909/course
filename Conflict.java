ec0f7410c140f6726d384a82e982edf6e2cf64fa
Alexei909 <vegetables912@gmail.com>
2024-06-08 11:37:36 +0500

    add Conflict.java

---
+++ Conflict.java
@@ -0,0 +1,20 @@
+public class Conflict {
+    
+    public static void main(String[] args) {
+        print1();
+        print2();
+        print3();
+    }
+
+    private static void print1() {
+        System.out.println(1);
+    }
+
+    private static void print2() {
+        System.out.println(2);
+    }
+
+    private static void print3() {
+        System.out.println(3);
+    }
+}
